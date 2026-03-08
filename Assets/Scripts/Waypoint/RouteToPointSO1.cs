9475c5a489222a7cc757b0a0a6dda5361c13dd15
Redprix <111867344+Redprix@users.noreply.github.com>
2026-03-08 07:53:46 +0700

    adjustments

--- Assets/Scripts/Waypoint/RouteToPointSO1.cs
+++
@@ -1,9 +0,0 @@
-//using UnityEngine;
-
-//[CreateAssetMenu(menuName = "Waypoint/Route 2 Point (Position)", fileName = "Route2PointPos")]
-
-//public class RouteToPointSO
-//{
-//    public Vector3 pointA;
-//    public Vector3 pointB;
-//}
